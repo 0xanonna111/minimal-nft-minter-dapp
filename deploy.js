fed3a0959f984f85b0003e86156f127c750ef354
Anonna <136353692+0xanonna111@users.noreply.github.com>
2026-02-18 15:26:27 +0600

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const NFT = await hre.ethers.getContractFactory("MyNFT");
+  const nft = await NFT.deploy();
+
+  await nft.waitForDeployment();
+
+  console.log("NFT deployed to:", await nft.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
